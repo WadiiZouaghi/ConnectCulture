6af9a9b626a45bd422b2594f0c3a3238a835eef4
ASSETOU124 <assetougogo@gmail.com>
2025-02-17 11:56:51 +0100

    front back
    ajouter front et back

---
+++ src/Entity/Competition.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompetitionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CompetitionRepository::class)]
+class Competition
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $Id_comp = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Date_debut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $Date_fin = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Etat = null;
+
+    #[ORM\Column]
+    private ?int $Organisateur_ID = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdComp(): ?int
+    {
+        return $this->Id_comp;
+    }
+
+    public function setIdComp(int $Id_comp): static
+    {
+        $this->Id_comp = $Id_comp;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): static
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->Date_debut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $Date_debut): static
+    {
+        $this->Date_debut = $Date_debut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->Date_fin;
+    }
+
+    public function setDateFin(\DateTimeInterface $Date_fin): static
+    {
+        $this->Date_fin = $Date_fin;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->Etat;
+    }
+
+    public function setEtat(?string $Etat): static
+    {
+        $this->Etat = $Etat;
+
+        return $this;
+    }
+
+    public function getOrganisateurID(): ?int
+    {
+        return $this->Organisateur_ID;
+    }
+
+    public function setOrganisateurID(int $Organisateur_ID): static
+    {
+        $this->Organisateur_ID = $Organisateur_ID;
+
+        return $this;
+    }
+}
