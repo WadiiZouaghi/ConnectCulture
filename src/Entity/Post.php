21bca83e999e9a36541ff5eb4ed0607a387c6c8e
yassine <chbinouyassine@gmail.com>
2025-02-20 12:33:21 +0100

    gestionPost

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,24 +4,59 @@ namespace App\Entity;
 
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ORM\Entity]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'integer')]
+    private $idPost;
 
-    public function getId(): ?int
+    #[ORM\Column(type: 'string', length: 255)]
+    private $texte;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateCreation;
+
+    // Getters and setters for the properties
+    public function getIdPost(): ?int
     {
-        return $this->id;
+        return $this->idPost;
     }
 
-    public function setId(int $id): static
+    public function getTexte(): ?string
     {
-        $this->id = $id;
+        return $this->texte;
+    }
 
+    public function setTexte(string $texte): self
+    {
+        $this->texte = $texte;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
         return $this;
     }
 }
---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Panier;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Panier>
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Panier::class);
+    }
+
+    /**
+     * Trouver un panier d'un utilisateur spécifique.
+     *
+     * @param int $userId
+     * @return Panier|null
+     */
+    public function findByUserId(int $userId): ?Panier
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.user = :userId')
+            ->setParameter('userId', $userId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * Compter le nombre d'articles dans un panier.
+     *
+     * @param int $panierId
+     * @return int
+     */
+    public function countItemsInPanier(int $panierId): int
+    {
+        return $this->createQueryBuilder('p')
+            ->select('COUNT(pi)')
+            ->join('p.items', 'pi')  // Assuming you have a relationship with a 'items' entity
+            ->where('p.id = :panierId')
+            ->setParameter('panierId', $panierId)
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    /**
+     * Récupérer un panier par son ID.
+     *
+     * @param int $panierId
+     * @return Panier|null
+     */
+    public function findOneById(int $panierId): ?Panier
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.id = :panierId')
+            ->setParameter('panierId', $panierId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
